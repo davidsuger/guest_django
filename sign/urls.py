216d8909b5abba83d734c9d6ff6aace36a2211e9
QuanLi <quanlidavid@gmail.com>
2017-10-26 09:04:53 +0800

    updated to charpter 9

--- sign/urls.py
+++ sign/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from sign import views_if
+from sign import views_if_sec
 
 urlpatterns=[
     url(r'^add_event/',views_if.add_event, name='add_event'),
     url(r'^add_guest/',views_if.add_guest, name='add_guest'),
     url(r'^get_event_list/',views_if.get_event_list, name='get_event_list'),
-    # url(r'^get_guest_list/',views_if.get_guest_list, name='get_guest_list'),
-    # url(r'^user_sign/',views_if.user_sign, name='user_sign'),
+    url(r'^get_guest_list/',views_if.get_guest_list, name='get_guest_list'),
+    url(r'^user_sign/',views_if.user_sign, name='user_sign'),
+    url(r'^sec_get_event_list/',views_if_sec.get_event_list, name='sec_get_event_list'),
+    url(r'^sec_add_event/',views_if_sec.add_event, name='sec_add_event'),
 ]
--- sign/views_if_sec.py
+++ sign/views_if_sec.py
@@ -1,13 +1,69 @@
-import time
+import time, base64, hashlib
 
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.db.utils import IntegrityError
 from django.http import JsonResponse
+from django.contrib import auth as django_auth
 
 from sign.models import Event, Guest
 
 
+def user_signature(request):
+    if request.method == 'POST':
+        client_time = request.POST.get('time', '')
+        client_sign = request.POST.get('sign', '')
+    else:
+        return 'error'
+
+    if client_time == '' or client_sign == '':
+        return 'sign null'
+
+    now_time = time.time()
+    server_time = str(now_time).split('.')[0]
+
+    time_difference = int(server_time) - int(client_time)
+    if time_difference >= 60:
+        return 'timeout'
+
+    md5 = hashlib.md5()
+    sign_str = client_time + '&Guest-Bugmaster'
+    sign_bytes_utf8 = sign_str.encode(encoding='utf-8')
+    md5.update(sign_bytes_utf8)
+    server_sign = md5.hexdigest()
+
+    if server_sign != client_sign:
+        return 'sign fail'
+    else:
+        return 'sign success'
+
+
+def user_auth(request):
+    get_http_auth = request.META.get('HTTP_AUTHORIZATION', 'b')
+    auth = get_http_auth.split()
+    try:
+        auth_parts = base64.b64decode(auth[1]).decode('utf-8').partition(':')
+    except IndexError:
+        return 'null'
+    username, password = auth_parts[0], auth_parts[2]
+    user = django_auth.authenticate(username=username, password=password)
+    if user is not None:
+        django_auth.login(request, user)
+        return 'success'
+    else:
+        return 'fail'
+
+
 def add_event(request):
+    sign_result = user_signature(request)
+    if sign_result == 'error':
+        return JsonResponse({'status': 10011, 'message': 'request error'})
+    elif sign_result == 'sign null':
+        return JsonResponse({'status': 10012, 'message': 'user sign null'})
+    elif sign_result == 'timeout':
+        return JsonResponse({'status': 10013, 'message': 'user sign timeout'})
+    elif sign_result == 'sign fail':
+        return JsonResponse({'status': 10014, 'message': 'user sign error'})
+
     eid = request.POST.get('eid', '')
     name = request.POST.get('name', '')
     limit = request.POST.get('limit', '')
@@ -34,6 +90,12 @@ def add_event(request):
 
 
 def get_event_list(request):
+    auth_result = user_auth(request)
+    if auth_result == 'null':
+        return JsonResponse({'status': 10011, 'message': 'user auth null'})
+    if auth_result == 'fail':
+        return JsonResponse({'status': 10012, 'message': 'user auth fail'})
+
     eid = request.GET.get('eid', '')
     name = request.GET.get('name', '')
 
